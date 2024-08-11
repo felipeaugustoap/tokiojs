e0691fa3ef419b8c4501141755d68ec64c540564
Felipe Augusto <felipeaap9@gmail.com>
2024-08-11 15:14:04 +0200

    pratica 2

---
+++ praticas/pratica_2/main.js
@@ -0,0 +1,8 @@
+const radio = prompt("Indiquenos el valor de radio: ")
+
+const formula = "4/3 * PI * radio4 / 3 * Math.PI * Math.pow(radio, 3)"
+
+const resultado = 4 / 3 * Math.PI * Math.pow(radio, 3)
+
+alert(`El volumen de la esfera se calcula con la fórmula:  ${formula}, por tanto una esfera
+    de radio ${radio} tiene un volumen de ${resultado}`)
