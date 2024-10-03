ba01dd038b37951159c310c6732428a1846c117c
Felipe Augusto <felipeaap9@gmail.com>
2024-10-03 14:28:30 +0200

    pratica 4

--- praticas/pratica_4/main.js
+++ praticas/pratica_4/main.js
@@ -5,11 +5,13 @@ const nota4 = prompt("Idiquenos la cuarta nota: ")
 const nota5 = prompt("Idiquenos la quinta nota: ")
 const nota6 = prompt("Idiquenos la sexta nota: ")
 
-const media = Math.round((Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4) + Number(nota5) + Number(nota6)) / 6);
+
+
+const media = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4) + Number(nota5) + Number(nota6)) / 6;
 
 const calculoMedia = () => {
     
-    const resultado_media = media
+    const resultado_media = media.toFixed(2)
     
     
     const butonMedia = document.getElementById("resultado1")
@@ -23,17 +25,17 @@ const calculoMedia = () => {
 const clasificacionMedia = () => {
     const button2 = document.getElementById("resultado2")
     if (media >= 0 && media <= 2.99) {
-        button2.innerHTML = `El aluno ha sacado una media de ${media} que corresponde a un Muy deficiente`
+        button2.innerHTML = `El aluno ha sacado una media de ${media.toFixed(2)} que corresponde a un Muy deficiente`
     } else if (media >= 3 && media <= 4.99) {
-        button2.innerHTML = `El aluno ha sacado una media de ${media} que corresponde a un Insuficiente`
+        button2.innerHTML = `El aluno ha sacado una media de ${media.toFixed(2)} que corresponde a un Insuficiente`
     } else if (media >= 5 && media <= 5.99) {
-        button2.innerHTML = `El aluno ha sacado una media de ${media} que corresponde a un Suficiente`
+        button2.innerHTML = `El aluno ha sacado una media de ${media.toFixed(2)} que corresponde a un Suficiente`
     } else if (media >= 6 && media <= 6.99) {
-        button2.innerHTML = `El aluno ha sacado una media de ${media} que corresponde a un Bien`
+        button2.innerHTML = `El aluno ha sacado una media de ${media.toFixed(2)} que corresponde a un Bien`
     } else if (media >= 7 && media <= 8.99) {
-        button2.innerHTML = `El aluno ha sacado una media de ${media} que corresponde a un Notable`
+        button2.innerHTML = `El aluno ha sacado una media de ${media.toFixed(2)} que corresponde a un Notable`
     } else if (media >= 9 && media <= 10){
-        button2.innerHTML = `El aluno ha sacado una media de ${media} que corresponde a un Sobresaliente`
+        button2.innerHTML = `El aluno ha sacado una media de ${media.toFixed(2)} que corresponde a un Sobresaliente`
     }
 }
 
